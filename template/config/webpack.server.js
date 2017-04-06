2822531c5ebe43ec958cffb5d6fad02fb18dbc82
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-04-06 10:39:28 +0200

    reload

--- template/config/webpack.server.js
+++ template/config/webpack.server.js
@@ -1,1 +1,13 @@
-module.exports = {};
+/**
+ * Development Server Config
+ */
+const address = require('ip').address();
+const localhost = require('./hosts').local;
+
+module.exports = {
+    entry: {
+        app: [
+            'webpack-dev-server/client?http://' + address + ':' + localhost.port
+        ]
+    }
+};
