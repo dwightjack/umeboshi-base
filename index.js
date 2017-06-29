8ebffb3189e9c36fcb777e0038f47b15fdde4100
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-06-29 15:42:31 +0200

    update to the latest umeboshi-cli entrypoint format

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 module.exports = () => {
+    console.error('This entrypoint format is deprecated. Please upgrade umeboshi-cli.');
+    console.error();
+    console.error('npm install -g umeboshi-cli');
     return {};
 };
